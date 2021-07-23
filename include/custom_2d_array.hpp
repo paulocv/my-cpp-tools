d97403946a328fc45ad70af5e4184f65ccbe8408
paulocv <paulo.pc.vs@gmail.com>
2021-07-23 15:11:19 -0300

    Add size_t inside a namespace.

--- include/custom_2d_array.hpp
+++ include/custom_2d_array.hpp
@@ -16,6 +16,9 @@ Adapted from: https://www.learncpp.com/cpp-tutorial/template-classes/
 
 
 // DEV TODO: if I have guts, change the type of the size from int to another template parameter.
+namespace custom2darray{
+    using size_t = long int;
+}
 
 /*
 Homemade two-dimensional array, contiguous in row-major (column index varies first). Usies C++ 
@@ -34,15 +37,16 @@ template <class T>
 class Custom2DArray
 {
 private:
-    int num_rows_{};
-    int num_cols_{};
-    int length_{};
+
+    custom2darray::size_t num_rows_{};
+    custom2darray::size_t num_cols_{};
+    custom2darray::size_t length_{};
     T *data_{};
 
     // Used both at the constructor and the rebuild() member function. 
     // If called out of context, could leave old data allocated withouht pointer (a memory leak).
     // That's why it's private.
-    void build(int num_rows, int num_cols){
+    void build(custom2darray::size_t num_rows, custom2darray::size_t num_cols){
         num_rows_ = num_rows;
         num_cols_ = num_cols;
         length_ = num_rows * num_cols;
@@ -54,7 +58,7 @@ public:
     // --- Constructors
 
     // Construction of a 2D array with given dimensions (rows, cols).
-    Custom2DArray(int num_rows, int num_cols){
+    Custom2DArray(custom2darray::size_t num_rows, custom2darray::size_t num_cols){
         build(num_rows, num_cols);
     }
 
@@ -86,7 +90,7 @@ public:
     }
 
     // Removes previous data and reallocates with new dimensions. 
-    void rebuild(int num_rows, int num_cols){
+    void rebuild(custom2darray::size_t num_rows, custom2darray::size_t num_cols){
         erase();
         build(num_rows, num_cols);
     }
@@ -94,28 +98,36 @@ public:
     // --- Indexing operations 
 
     // Indexing with (i, j): returns the (i, j) entry of the 2D array.
-    T& operator()(int i, int j){
+    T& operator()(custom2darray::size_t i, custom2darray::size_t j){
         assert(i >= 0 && i < num_rows_);
         assert(j >= 0 && j < num_cols_);
         return data_[num_cols_ * i + j];
     }
 
     // Indexing with [m]: returns the m-th entry of the flattened 1D array.
-    T& operator[](int index)
+    T& operator[](custom2darray::size_t index)
     {
         assert(index >= 0 && index < length_);
         return data_[index];
     }
  
     // templated get_length() function defined below, just to examplify
-    int get_length() const;
-    int get_num_rows() const {return num_rows_;}
-    int get_num_cols() const {return num_cols_;}
+    custom2darray::size_t get_length() const;
+    custom2darray::size_t get_num_rows() const {return num_rows_;}
+    custom2darray::size_t get_num_cols() const {return num_cols_;}
+
+
+    // --- Other functionalities
+    void set_all_values_to(T val){
+        for (custom2darray::size_t m{0}; m < length_; m++){
+            data_[m] = val;
+        }
+    }
 };
  
 // member functions defined outside the class need their own template declaration
 template <class T>
-int Custom2DArray<T>::get_length() const // note class name is Array<T>, not Array
+custom2darray::size_t Custom2DArray<T>::get_length() const // note class name is Array<T>, not Array
 {
   return length_;
 }
